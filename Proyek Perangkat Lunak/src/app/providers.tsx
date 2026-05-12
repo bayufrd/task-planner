5ec5b7ee305ef1fbd53bf663d86ebcaaad4d0481
bayufrd <bayu.farid36@gmail.com>
2026-05-13 06:43:23 +0700

    fix: configure frontend providers and backend environment

--- Proyek Perangkat Lunak/src/app/providers.tsx
+++ Proyek Perangkat Lunak/src/app/providers.tsx
@@ -5,6 +5,7 @@ import { LanguageProvider } from '@/components/providers/LanguageProvider'
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
 import { CommandPaletteProvider } from '@/components/providers/CommandPaletteProvider'
 import { SnackbarProvider } from '@/components/providers/SnackbarProvider'
+import { AuthSessionProvider } from '@/components/providers/AuthSessionProvider'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
@@ -12,7 +13,9 @@ export function Providers({ children }: { children: ReactNode }) {
       <LanguageProvider>
         <ThemeProvider>
           <CommandPaletteProvider>
-            {children}
+            <AuthSessionProvider>
+              {children}
+            </AuthSessionProvider>
           </CommandPaletteProvider>
         </ThemeProvider>
       </LanguageProvider>
